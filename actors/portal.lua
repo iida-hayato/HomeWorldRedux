6b8406c77058279149f9119eb19abe265bc58d7a
hayato.iida <hayato.iida.0213@gmail.com>
2018-04-08 17:49:25 +0900

    debug

--- actors/portal.lua
+++ actors/portal.lua
@@ -3,21 +3,21 @@ Portal = Actor{name = "portal", use_entity_gui = true}
 local config = homeworld_config.portal
 
 function Portal:tick( tick )
-   if ModuloTimer(config.update_interval) then
-      local inventory = self.state.entity.get_inventory(1)
-      if inventory.is_empty() then
-        return
-      end
-      for index = 1, #inventory do
-         if inventory[index] and inventory[index].valid_for_read then
-            Homeworld:insert_item(inventory[index])
-            if not config.test_mode then
-               inventory.remove(inventory[index])
-            end
-         end
-      end
-      self:do_fx()
-   end
+   -- if ModuloTimer(config.update_interval) then
+   --    local inventory = self.state.entity.get_inventory(1)
+   --    if inventory.is_empty() then
+   --      return
+   --    end
+   --    for index = 1, #inventory do
+   --       if inventory[index] and inventory[index].valid_for_read then
+   --          Homeworld:insert_item(inventory[index])
+   --          if not config.test_mode then
+   --             inventory.remove(inventory[index])
+   --          end
+   --       end
+   --    end
+   --    self:do_fx()
+   -- end
 end
 
 function Portal:do_fx()
